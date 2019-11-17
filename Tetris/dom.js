71d1d0e097af5bafbd5ac8a82f6a575f8024c717
stampyzfanz <34364128+stampyzfanz@users.noreply.github.com>
2019-11-17 13:31:33 +1100

    Add dom; speed slider and reset button
    Change title of html page to 'Tetris' from 'Code!'
    Add reset button.
    Add slider that impacts the speed of the game.
    It works with inverse quadratic growth, because the default linear
    growth has the first 20% being really important and the rest boring.
    This way you can be less precise with your mouse to pick the option
    or probably want.
    
    Add reset button.

---
+++ Tetris/dom.js
@@ -0,0 +1,25 @@
+function setupDom() {
+	createElement('br')
+		.parent('#canvas');
+
+	let speed = createSlider(0, Math.sqrt(1024), Math.sqrt(256), 0.01)
+		.parent('#canvas')
+		.style('display', 'inline-block')
+		.style('width', '100%')
+		.input(_ => moveInterval = speed.value() ** 2);
+
+	let space = createP('&nbsp'.repeat(4))
+		.style('display', 'inline-block')
+		.parent('#canvas');
+
+	let explanation = createP('Speed of the tetrominoes falling')
+		.style('display', 'inline-block')
+		.parent('#canvas');
+
+	createElement('br')
+		.parent('#canvas');
+
+	let resetBtn = createButton('reset')
+		.mousePressed(reset)
+		.parent('#canvas');
+}
--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -37,6 +37,7 @@ function setup() {
 	}
 
 	findTetrominoTypes();
+	setupDom();
 }
 
 function draw() {
